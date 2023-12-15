1f586bfe4f20a9d9f11276fc0ae69ff4f8a42874
David <dbellenberg@live.de>
2023-12-15 22:24:13 +0100

    added models

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,33 +1,37 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def load_data(config,split=False, test_size=0.2, val_size=0.1):
-    """
-    Load data from a file and optionally split into train, validation, and test sets.
 
-    :param file_path: Path to the data file.
+def load_data(config, split=False, test_size=0.2, val_size=0.1):
+    """
+    Load data from a file, separate features and target, and optionally split into train, validation, and test sets.
+
+    :param config: Configuration dictionary with file path.
+    :param target_column: Name of the target variable column.
     :param split: Boolean, whether to split the data or not.
     :param test_size: Proportion of the dataset to include in the test split.
     :param val_size: Proportion of the training dataset to include in the validation split.
-    :return: Depending on the 'split', returns either full dataset or split datasets.
+    :return: Depending on the 'split', returns either full dataset or split datasets along with their targets.
     """
-    #path /Users/davidbellenberg/github_projects/Loan-Risk-Optimizer/data/heloc.csv
-
     # Get the file path from the config file
-
     file_path = config["dataset"]["data_path"]
 
     # Load the dataset
     data = pd.read_csv(file_path)
+
+    # Separate features and target
+    X = data.drop("RiskPerformance", axis=1)
+    y = data["RiskPerformance"]
 
     if split:
         # Calculate validation size with respect to the original dataset
         val_size_adjusted = val_size / (1 - test_size)
         
         # Splitting the data
-        train_data, test_data = train_test_split(data, test_size=test_size, random_state=42)
-        train_data, val_data = train_test_split(train_data, test_size=val_size_adjusted, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_size_adjusted, random_state=42)
 
-        return train_data, val_data, test_data
+        return X_train, X_val, X_test, y_train, y_val, y_test
 
-    return data
+    return X, y
+
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,8 @@
 import os
 import yaml
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, auc, classification_report, confusion_matrix, roc_curve
+import seaborn as sns
 
 def load_config():
     """
@@ -16,3 +19,22 @@ def load_config():
         config = yaml.safe_load(file)
     
     return config
+
+# Evaluation function that prints classification report and confusion matrix
+def evaluate(y_test, y_pred, model_name):
+    #print accuracy
+    print(f"Accuracy: {accuracy_score(y_test, y_pred):.2f}")
+    print()
+    cm = confusion_matrix(y_test, y_pred)
+
+    print(f"\033[34m{model_name}\033[0m")
+    print()
+    print(f" \033[32mClassification Report:\033[0m")
+    print(classification_report(y_test, y_pred))
+    print(f" \033[32mConfusion Matrix:\033[0m")
+    
+    sns.heatmap(cm, annot=True, cmap='Blues', fmt='g')
+    plt.title(f'{model_name}: Confusion Matrix')
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.show()
